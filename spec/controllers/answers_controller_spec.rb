4b7aaa9a3047fb4d06f4d394d7710822929c737c
Alex A <ownmymail@yandex.ru>
2016-08-17 19:13:31 +0300

    Add the tests for answers

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
 
+  describe 'GET #new' do
+    before { get :new, params: { question_id: question } }
+
+    it 'assigns the requested question to @question' do      
+      expect(assigns(:answer)).to be_a_new(Answer)      
+    end
+
+    it 'renders a new view' do
+      expect(request).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'add a new item into datebase' do
+        expect{ post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(question.answers, :count).by(1)
+      end
+
+      it 'redirect to view question' do
+        post :create , params: { question_id: question, answer: attributes_for(:answer) }
+        expect(request).to redirect_to question
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'do not add a new item into datebase' do
+        expect{ post :create, params: { question_id: question, answer: attributes_for(:invalid_answer) } }.to_not change(question.answers, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, params: { question_id: question, answer: attributes_for(:invalid_answer) } 
+        expect(request).to render_template :new
+      end
+    end
+  end
 end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
   factory :answer do
     content "text text"
-    question
+    question  
+  end
+
+  factory :invalid_answer, class: "Answer" do	
+  	content ""
+    question 
   end
 end
